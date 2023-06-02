300e901a54c5c0655c7b87919b309b90a738a01f
Your Name <you@example.com>
2023-06-02 16:09:01 +0300

    new blok

---
+++ src/components/BoardsCardTask/BoardsCardTaskState.jsx
@@ -0,0 +1,13 @@
+const boardCardTaskState =[
+    {
+        title:"Send status updates back through Intercom"
+    },
+    {
+        number:'12'
+    },
+    {
+        icon1:"/src/assets/image/dotsIcon1.svg",
+        icon2:"/src/assets/image/dotsIcon2.svg",
+    }
+]
+export default  boardCardTaskState
--- src/pages/Boards/Boards.jsx
+++ src/pages/Boards/Boards.jsx
@@ -3,6 +3,7 @@ import boasrdState from "./BoardsState";
 import s from "./Board.module.css"
 import TitlePages from "../../components/TitlePages/TitlePages";
 import BlueBtn from "../../components/Buttons/BlueBtn/BlueBtn";
+import BoardCards from "../../components/BoardsCardTask/BoardCards/BoardCards";
 const Boards = (props) => {
     return ( 
         <div className={s.main}>
@@ -15,6 +16,9 @@ const Boards = (props) => {
                 <span className={s.info__name}>{boasrdState[0].name}</span>
                 <span className={s.info__name}>{boasrdState[0].posts}</span>
 
+            </div>
+            <div className={s.boards__task}>
+                <BoardCards/>
             </div>
         </div>
             
