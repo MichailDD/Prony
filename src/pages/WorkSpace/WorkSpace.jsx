cc7ea8864b4330b83008d7d8382a1a386241afc2
Danil <danil.polovinkin.1994@gmail.com>
2023-06-08 14:55:25 +0300

    completed roadMap section

--- src/pages/WorkSpace/WorkSpace.jsx
+++ src/pages/WorkSpace/WorkSpace.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeaderWork from "../../components/HeaderWork/HeaderWork";
 import "./workSpace.module.scss";
 import InputSec from "../../components/InputWork/InputSec";
+import RoadMap from "../../components/RoadMap/RoadMap";
 
 const WorkSpace = () => {
   return (
     <>
       <HeaderWork />
       <InputSec />
+      <RoadMap />
     </>
   );
 };
