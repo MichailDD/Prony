96860ac7777921aad25be0fd92c48d4d33c99510
Danil <danil.polovinkin.1994@gmail.com>
2023-06-08 12:11:36 +0300

    completed header , stateless

---
+++ src/pages/WorkSpace/WorkSpace.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderWork from "../../components/HeaderWork/HeaderWork";
+import "./workSpace.module.scss";
+
+const WorkSpace = () => {
+  return (
+    <>
+      <HeaderWork />
+    </>
+  );
+};
+
+export default WorkSpace;
