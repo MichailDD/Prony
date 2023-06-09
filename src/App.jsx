e0788ccc38bb052eb4bca9f2ce9e0e154ffaf510
Your Name <you@example.com>
2023-06-09 10:48:17 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,8 @@ import CreatePost from "./pages/Posts/CreatePost/CreatePost";
 import Layoute from "./components/LayoutPage/LayoutePage";
 import Boards from "./pages/Boards/Boards";
 import Posts from "./pages/Posts/Posts";
-<<<<<<< HEAD
-import VotersPosts from "./pages/Posts/VotersPosts/VotersPosts";
-import Tags from "./pages/Tags/Tags";
-const App = (props) => {
-  return ( 
-    <>
-      <Routes>
-        <Route path="/" element={<Layoute/>}>
-        <Route index path="/" element={<DashBoard/>}/>
-        <Route path="/boards" element={<Boards/>}/>
-        <Route path="/posts" element={<Posts/>}/>
-        <Route path="/createPosts" element={<CreatePost/>}/>
-        <Route path="/votersPosts" element={<VotersPosts/>}/>
-        <Route path="/tags" element={<Tags/>}/>
-=======
 import WorkSpace from "./pages/WorkSpace/WorkSpace";
-
+import VotersPosts from "./pages/Posts/VotersPosts/VotersPosts";
 const App = () => {
   return (
     <>
@@ -31,10 +16,10 @@ const App = () => {
           <Route path="/boards" element={<Boards />} />
           <Route path="/posts" element={<Posts />} />
           <Route path="/createPosts" element={<CreatePost />} />
+          <Route path="/votersPosts" element={<VotersPosts />} />
         </Route>
         <Route>
           <Route path="/WorkSpacePage" element={<WorkSpace />} />
->>>>>>> 61b2ddde0456d36978f4d94d4f2a00c1e955506d
         </Route>
       </Routes>
     </>
