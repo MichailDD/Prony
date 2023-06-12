db5c0cacbfbe03bb8884e1ccc6e8d11dd9fb5e12
Your Name <you@example.com>
2023-06-12 15:32:32 +0300

    Finish Users

--- src/components/UserDatails/UserDatails.jsx
+++ src/components/UserDatails/UserDatails.jsx
@@ -1,11 +1,14 @@
 import UserDatailsHeader from "./UserDatailsHeader/UserDatailsHeader";
 import s from "./UserDatails.module.css"
+import UserDatailsPosts from "./UserDatailsPosts/UserDatailsPosts";
 const UserDatails = (props) => {
     return (  
         <div className={s.main}>
             <div className={s.main__board}>
                      <UserDatailsHeader/>
-
+                     <div className={s.main__posts}>
+                        <UserDatailsPosts/>
+                     </div>
             </div>
         </div>
     );
