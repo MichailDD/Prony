d44583f97f1f1c82cdde27e7283bcc366bd69eb8
Danil <danil.polovinkin.1994@gmail.com>
2023-06-14 12:44:07 +0300

    completed registration

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Avatar from "./pages/WorkSpace/Avatar/Avatar";
 import Error404 from "./pages/WorkSpace/404/error404";
 import Error505 from "./pages/WorkSpace/505/Error505";
 import Login from "./pages/WorkSpace/login/Login";
+import Registration from "./pages/WorkSpace/registration/Registration";
 
 const App = () => {
   return (
@@ -37,6 +38,7 @@ const App = () => {
           <Route path="ChangeProfile" element={<Profile />} />
           <Route path="ChangeAvatar" element={<Avatar />} />
           <Route path="Login" element={<Login />} />
+          <Route path="Registration" element={<Registration />} />
           <Route path="404" element={<Error404 />} />
           <Route path="500" element={<Error505 />} />
         </Route>
