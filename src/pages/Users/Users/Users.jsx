6dfd350359911bfec23f6202d7528f76903938d6
Your Name <you@example.com>
2023-06-16 12:32:43 +0300

    new page"

--- src/pages/Users/Users/Users.jsx
+++ src/pages/Users/Users/Users.jsx
@@ -1,8 +1,17 @@
+import TitlePages from "../../../components/TitlePages/TitlePages";
+import UserList from "../../../components/UserList/UserList";
 import s from "./Users.module.css"
 const Users = (props) => {
     return ( 
-        <div>
-            ffff
+        <div className={s.main}>
+            <div className={s.main__wrapper}>
+                <div className={s.main__title}>
+                <TitlePages title="Users"/>
+                </div>
+                <div className={s.main__board}>
+                    <UserList/>
+                </div>
+            </div>
         </div>
      );
 }
